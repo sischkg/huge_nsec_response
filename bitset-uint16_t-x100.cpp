f3864716024cfea7d0bf4e30b50f81905e1539ff
Toshifumi Sakaguchi <t-sakaguchi@fusioncom.co.jp>
2019-11-28 10:25:21 +0900

    add bitset code

---
+++ bitset-uint16_t-x100.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <bitset>
+#include <iostream>
+#include <unistd.h>
+
+// $ g++ -std=c++11 bitset-uint16_t-x100.cpp
+// $ ps -C a.out -o rss,cmd
+//   RSS CMD
+//   311268 ./a.out
+
+typedef std::bitset<65536> BitMaps;
+
+int main()
+{
+        std::vector<BitMaps> bitmaps;
+        for ( unsigned int j = 0 ; j < 100 ; j++ ) {
+                BitMaps bitmap;
+                for ( uint16_t i = 0 ; i < 0xffff ; i++ ) {
+                        bitmap.set( i );
+                }
+                bitmaps.push_back( bitmap );
+        }
+
+        sleep( 60 );
+        return 0;
+}
